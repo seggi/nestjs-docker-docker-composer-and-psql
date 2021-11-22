f7e9e1ce0aee3db0b5dadc64e6304b7eba08c2c2
seggi <seggimarugira@gmail.com>
2021-11-21 21:00:51 -0500

    config-sequilize

--- entrypoint.bash
+++ entrypoint.bash
@@ -1,2 +1,2 @@
 #!/bin/sh
-yarn serve
+yarn start
---
+++ src/database/migrations/20211121210234-create-user.js
@@ -0,0 +1,105 @@
+import { Model } from 'sequelize';
+
+const PROTECTED_ATTRIBUTES = ['password'];
+
+export default (sequelize, DataTypes) => {
+  class User extends Model {
+    toJSON() {
+      // hide protected fields
+      const attributes = { ...this.get() };
+      // eslint-disable-next-line no-restricted-syntax
+      for (const a of PROTECTED_ATTRIBUTES) {
+        delete attributes[a];
+      }
+      return attributes;
+    }
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  User.init({
+    name: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter your email address',
+      },
+      unique: {
+        args: true,
+        msg: 'Email already exists',
+      },
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'Please enter a valid email address',
+        },
+      },
+    },
+    phone: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    password: DataTypes.STRING,
+    status: DataTypes.STRING,
+    last_login_at: DataTypes.DATE,
+    last_ip_address: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
+and
+export default {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      phone: {
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      last_login_at: {
+        type: Sequelize.DATE
+      },
+      last_ip_address: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
