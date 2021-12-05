51caa73d2a09ba576d5812ecdbd2815e1bfe96d8
seggi <seggimarugira@gmail.com>
2021-12-04 23:21:50 -0500

    config some changes

--- src/models/user.js
+++ src/models/user.js
@@ -1,52 +1,12 @@
-import { Model } from 'sequelize';
+import { DataTypes, Model, Optional } from "sequelize/dist";
+import sequelizeConnection from '../config';
 
-const PROTECTED_ATTRIBUTES = ['password'];
-
-export default (sequelize, DataTypes) => {
-  class User extends Model {
-    toJSON() {
-      // hide protected fields
-      const attributes = { ...this.get() };
-      for (const a of PROTECTED_ATTRIBUTES) {
-        delete attributes[a];
-      }
-      return attributes;
-    }
-    static associate(models) {
-
-    }
-  };
-  User.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: {
-      type: DataTypes.STRING,
-      allowNull: {
-        args: false,
-        msg: 'Please enter your email address',
-      },
-      unique: {
-        args: true,
-        msg: 'Email already exists',
-      },
-      validate: {
-        isEmail: {
-          args: true,
-          msg: 'Please enter a valid email address',
-        },
-      },
-    },
-    phone: {
-      type: DataTypes.STRING,
-      unique: true,
-    },
-    password: DataTypes.STRING,
-    status: DataTypes.STRING,
-    last_login_at: DataTypes.DATE,
-    last_ip_address: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
-  return User;
+interface UserAttributes {
+  id: number;
+  first_name: string;
+  last_name: string;
+  age: number;
+  gender: string;
+  createAt?: Date;
+  updateAt?: Date;
 };
