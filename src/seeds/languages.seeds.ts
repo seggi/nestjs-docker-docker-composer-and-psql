b598ee65c3828660f6d0d35c342145a76b729724
seggi <seggimarugira@gmail.com>
2022-01-04 17:28:04 -0500

    config seeds with typeOrm

---
+++ src/seeds/languages.seeds.ts
@@ -0,0 +1,17 @@
+import { Languages } from "../model/languages.entity";
+import { Connection } from "typeorm";
+import { Factory, Seeder } from "typeorm-seeding";
+
+export default class CreateLanguages implements Seeder {
+    public async run(factory: Factory, connection: Connection): Promise<any> {
+        await connection
+            .createQueryBuilder()
+            .insert()
+            .into(Languages)
+            .values([
+                { name: "French" },
+                { name: "English" },
+            ])
+            .execute()
+    }
+}
